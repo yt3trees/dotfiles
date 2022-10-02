417eac4ebceb9d3cb44dfd83141abde5b6561104
yt3trees <y.atforest@gmail.com>
2022-10-02 18:44:05 +0900

    add

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,22 @@
+# Auto Completion
+try
+{
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionViewStyle ListView
+} catch
+{
+    Write-Warning "PSReadLine not installed."
+    Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+}
+
+# Tig
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 
 # mytheme.omp.jsonが存在しない場合はtokyo.omp.jsonをセットする
-oh-my-posh init pwsh --config "~/.mytheme.omp.json" | Invoke-Expression
-if (Test-Path -Path "~/.mytheme.omp.json") {
+if (Test-Path -Path "~/.mytheme.omp.json")
+{
     oh-my-posh init pwsh --config "~/.mytheme.omp.json" | Invoke-Expression
-}
-else {
+} else
+{
     oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tokyo.omp.json" | Invoke-Expression
 }
