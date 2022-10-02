e873530b9edaa399daece52c1809b842bc6f1399
yt3trees <y.atforest@gmail.com>
2022-10-02 15:35:16 +0900

    update

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,10 @@
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+
+# mytheme.omp.jsonが存在しない場合はtokyo.omp.jsonをセットする
+oh-my-posh init pwsh --config "~/.mytheme.omp.json" | Invoke-Expression
+if (Test-Path -Path "~/.mytheme.omp.json") {
+    oh-my-posh init pwsh --config "~/.mytheme.omp.json" | Invoke-Expression
+}
+else {
+    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tokyo.omp.json" | Invoke-Expression
+}
